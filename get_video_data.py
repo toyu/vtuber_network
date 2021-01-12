df6a6f06a1aa9338d642482391c306047a258452
toyu <20rmd21@ms.dendai.ac.jp>
2021-01-12 17:29:04 +0900

    Update get_video_data.py
    add comment

--- get_video_data.py
+++ get_video_data.py
@@ -7,18 +7,19 @@ members = pd.read_csv("data/hololiver_url.csv", header=True, encoding="utf_8", d
 YOUTUBE_API_KEY = 
 youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
-# for member in members.iterrows():
-#     with open('data/videos/20200601_20200813_50/' + str(member[1]["id"]) + '.pkl', 'rb') as web:
-#         videos_data = pickle.load(web)
-#
-#     data_list = []
-#     for video_data in videos_data['items']:
-#         search_response = youtube.videos().list(
-#             part='snippet',
-#             id=video_data['id']['videoId'],
-#             ).execute()
-#
-#         data_list.append(search_response)
-#
-#     with open('data/video/20200601_20200813_50/' + str(member[1]["id"]) + '.pkl', 'wb') as web:
-#         pickle.dump(data_list, web)
+# 対象Youtuberの動画データの情報取得
+for member in members.iterrows():
+    with open('data/videos/20200601_20200813_50/' + str(member[1]["id"]) + '.pkl', 'rb') as web:
+        videos_data = pickle.load(web)
+
+    data_list = []
+    for video_data in videos_data['items']:
+        search_response = youtube.videos().list(
+            part='snippet',
+            id=video_data['id']['videoId'],
+            ).execute()
+
+        data_list.append(search_response)
+
+    with open('data/video/20200601_20200813_50/' + str(member[1]["id"]) + '.pkl', 'wb') as web:
+        pickle.dump(data_list, web)
